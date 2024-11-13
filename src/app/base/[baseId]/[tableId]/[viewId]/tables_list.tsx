73375c8f89ada7c867836803da98b82ae8fbfa47
MrHawker <bonbon.muoiot@gmail.com>
2024-11-13 14:26:57 +1100

    fix weird display

--- src/app/base/[baseId]/[tableId]/[viewId]/tables_list.tsx
+++ src/app/base/[baseId]/[tableId]/[viewId]/tables_list.tsx
@@ -5,14 +5,20 @@ import { useParams, useRouter } from 'next/navigation'
 
 import { api } from "~/trpc/react";
 
+
 export function TableList(){
-    const params = useParams<{ baseId: string; tableId: string, viewId:string }>()
-    const { data: tables, isLoading } = api.common.getFirstViewForTables.useQuery({baseId:params.baseId})
-    const router = useRouter()
     
+    const params = useParams<{ baseId: string; tableId: string, viewId:string }>()
+    
+    const { data: tables, isLoading } = api.common.getFirstViewForTables.useQuery({baseId:params.baseId})
+    
+    const router = useRouter()
+    let exist = 0
+    const l = tables?.length ?? 0
     return <div className="flex">
         {tables?.map((table,index) =>{
           if(table.id == params.tableId){
+            exist = 1
             return (
               <div key={index} className="px-[12px] py-2 bg-white rounded-t text-black flex justify-center shadow-sm hover:cursor-pointer">
                 <p className="font-medium text-xs mr-1">Table {index+1}</p>
@@ -21,7 +27,10 @@ export function TableList(){
             )
           }else{
             return (
-              <div onClick={()=>router.push(`/base/${params.baseId}/${table.id}/${table.views.at(0)?.id}`)} key={index} className="flex">
+              <div onClick={()=>
+              router.push(`/base/${params.baseId}/${table.id}/${table.views.at(0)?.id}`)
+              
+              } key={index} className="flex">
                 <div  className="px-[12px] py-2 text-slate-200 font-thin flex justify-center shadow-sm hover:cursor-pointer">
                   <p className="font-medium text-xs mr-1">Table {index+1}</p>
                   <div className="flex flex-col justify-center"><RiArrowDownSLine /></div>
@@ -32,5 +41,11 @@ export function TableList(){
           }
             
         })}
+        { !exist &&
+          <div  className="px-[12px] py-2 bg-white rounded-t text-black flex justify-center shadow-sm hover:cursor-pointer">
+            <p className="font-medium text-xs mr-1">Table {l+1}</p>
+            <div className="flex flex-col justify-center"><RiArrowDownSLine /></div>
+          </div>
+        }
     </div>
 }
